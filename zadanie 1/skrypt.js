ffc0d9ad2124a5124a855246ebc479726c699f56
Zofia Grąbczewska (Uczeń) <zgrabczewska353@technikumkreatywne.pl>
2023-09-22 10:59:33 +0200

    nie umiem innego języka więc wykonałam to zadanie w js

---
+++ zadanie 1/skrypt.js
@@ -0,0 +1,10 @@
+var a = prompt ('podaj pierwsza liczbe')
+var b = prompt ('podaj druga liczbe')
+document.write ("Wynik odejmowania wiekszej od mniejszej to: ")
+
+if (a > b){
+    document.write (a - b)
+}
+else {
+    document.write (b - a)
+}
